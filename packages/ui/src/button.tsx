ff4324a2f646952e48381d0b16fbce2686fae063
Nazim Saifi <saifinazim2311@gmail.com>
2024-09-14 18:27:50 +0530

    Added tailwind

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,15 +3,14 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  children: ReactNode;
-  className?: string;
-  appName: string;
+  children: ReactNode,
+  appName:string
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, appName }: ButtonProps) => {
   return (
     <button
-      className={className}
+      className="bg-red-800 text-blue-900 p-2 text-xl rounded-md shadow-lg"
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
