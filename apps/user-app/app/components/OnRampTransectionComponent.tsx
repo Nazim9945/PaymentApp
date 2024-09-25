a6eaf11ac209ad76b26dbf827a1fca9017fa0d4e
Nazim Saifi <saifinazim2311@gmail.com>
2024-09-25 19:48:02 +0530

    Modified onRampTransaction file

--- apps/user-app/app/components/OnRampTransectionComponent.tsx
+++ apps/user-app/app/components/OnRampTransectionComponent.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
-enum onRampStatus{
-  Success,
-  Failure,
-  Processing
-}
-interface onRamp{
-  time:Date,
-  amount:number,
-  status:onRampStatus,
-  provider:string
+
+interface onRamp {
+  time: Date;
+  amount: number;
+  status:any;//type???
+  provider: string;
 }
 interface Props{
   onrampdetails:onRamp[]
+}
+enum OnRampStatus {
+  Success,
+  Failure,
+  Processing,
 }
 const OnRampTransectionComponent = ({onrampdetails}:Props) => {
   onrampdetails=onrampdetails.reverse()
@@ -28,7 +29,7 @@ const OnRampTransectionComponent = ({onrampdetails}:Props) => {
                 <div className="font-semibold">Recieved INR</div>
                 <div>{t.time.toDateString()}</div>
                 
-                <div className={`text-sm ${t.status==='Processing' && "text-red-800 italic"}`}>{t.status}</div>
+                <div className={`text-sm ${t.status===OnRampStatus.Processing && "text-red-800 italic"}`}>{t.status}</div>
               </div>
               <div>+ Rs {t.amount / 100}</div>
             </div>
--- apps/user-app/app/lib/actions/p2ptransfer.tsx
+++ apps/user-app/app/lib/actions/p2ptransfer.tsx
@@ -7,7 +7,6 @@ import prisma from "@repo/prismadb/client";
 
 export async function p2ptransfer(to:string,amount:number) {
    const session=await getServerSession(authoption)
-   console.log(session)
    const from =session.user.id
    if(!from){
     return {
@@ -19,39 +18,44 @@ export async function p2ptransfer(to:string,amount:number) {
         number:to
     }
    })
-    await  prisma.$transaction(async(tx)=>{
-        await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
-    const fromBalance=await tx.balance.findUnique({
-        where:{
-            userId:Number(from)
-        }
-    })
-    if(!fromBalance || (fromBalance.amount<amount)){
-        return {
-            message:"Insufficient Money"
-        }
-    }
-    await tx.balance.update({
-        where:{
-            userId:touser?.id
-        },
-        data:{
-            amount:{
-                increment:amount
-            }
-        },
-    })
-    await tx.balance.update({
-      where: {
-        userId: Number(from),
-      },
-      data: {
-        amount: {
-          decrement: amount,
-        },
-      },
-    });
-})
+   try {
+     await prisma.$transaction(async (tx) => {
+       await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
+       const fromBalance = await tx.balance.findUnique({
+         where: {
+           userId: Number(from),
+         },
+       });
+       if (!fromBalance || fromBalance.amount < amount) {
+         return {
+           message: "Insufficient Money",
+         };
+       }
+       await tx.balance.update({
+         where: {
+           userId: touser?.id,
+         },
+         data: {
+           amount: {
+             increment: amount,
+           },
+         },
+       });
+       await tx.balance.update({
+         where: {
+           userId: Number(from),
+         },
+         data: {
+           amount: {
+             decrement: amount,
+           },
+         },
+       });
+     });
+   } catch (error) {
+    console.log(error)
+    
+   }
 return {
     message:"Sent Successfully"
 }
